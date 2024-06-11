2f859ecbe7b0358af4069f3850c78462fefa7e5c
e_danilchenko <e_danilchenko@outlook.com>
2024-06-11 12:22:26 +0300

    Initial Commit

---
+++ weather/weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherApp.swift
+//  weather
+//
+//  Created by Yevhen on 11.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
